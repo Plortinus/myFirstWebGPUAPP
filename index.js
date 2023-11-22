48b2d39e3434746b6fa64373aeed20458c6f7961
wxy <wangxiaoyong@haier.net>
2023-11-22 16:56:32 +0800

    feat: add default http-server

--- index.js
+++ index.js
@@ -11,3 +11,31 @@ if (!adapter) {
 }
 // Create the device
 const device = await adapter.requestDevice();
+
+// Configure the canvas for WebGPU
+const context = canvas.getContext("webgpu");
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+context.configure({
+  device: device,
+  format: canvasFormat,
+});
+
+// Create a command encoder and pass it to the GPU
+const encoder = device.createCommandEncoder();
+
+// Create a render pass
+const pass = encoder.beginRenderPass({
+  colorAttachments: [{
+     view: context.getCurrentTexture().createView(),
+     loadOp: "clear",
+     storeOp: "store",
+  }]
+});
+
+// Finish the render pass
+pass.end();
+
+
+// Submit the command buffer to the GPU
+const commandBuffer = encoder.finish();
+device.queue.submit([encoder.finish()]);
